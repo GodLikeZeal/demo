61be6556d9b9fd876c44179c3d9f2c4349d84411
zeal <zhangleifor@163.com>
2018-03-20 00:08:38 +0800

    提交

---
+++ zealsay-common/src/main/java/com/zeal/zealsay/common/bean/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.zeal.zealsay.common.bean.config;
+
+
+import com.zeal.zealsay.common.bean.resolver.TokenArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+/**
+ * @author lengleng
+ * @date 2017/12/21
+ * mvc配置
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new TokenArgumentResolver(cacheManager));
+    }
+}
